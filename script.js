9581cc3e97707c2f7bb37a8dcda67ac95c1ab208
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-01-16 15:59:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
+ document.addEventListener('DOMContentLoaded', function () {
+  var msg = 'Sign up to receive newsletter for 10% off!';
+  var el = document.getElementById('message');
+  el.textContent = msg;
+});
