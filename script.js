69cb15f3bb47c5ef73a7d347778c90e9bf8636cb
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-01-20 16:49:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,34 @@ function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
 
+var expiryMsg;
+	var today = new Date();
+	var elEnds;
+
+	function offerExpires(today) {
+	  var weekFromToday, day, date, month, year, dayNames, monthNames;
+
+  // Calculate one week from today
+  weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+  // Day and month names
+  dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  // Extract details
+  day = dayNames[weekFromToday.getDay()];
+  date = weekFromToday.getDate();
+  month = monthNames[weekFromToday.getMonth()];
+  year = weekFromToday.getFullYear();
+
+  // Create the message
+  expiryMsg = `Offer expires next ${day}, ${month} ${date}, ${year}.`;
+
+  // Insert into the HTML element
+  elEnds = document.getElementById('offerEnds');
+  elEnds.textContent = expiryMsg;
+  }
+
+	// Call the function
+	offerExpires(today);
+
