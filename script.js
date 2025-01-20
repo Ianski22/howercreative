8c94889d55b367be549e7eef9bfe1731e5ae0a48
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-01-20 16:47:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ signInButton.addEventListener('click', () => {
   var el = document.getElementById('message');
   el.textContent = msg;
 });
+
+// JavaScript to dynamically display the greeting
+document.addEventListener("DOMContentLoaded", function () {
+  const today = new Date();
+  const hourNow = today.getHours();
+  let greeting;
+
+  if (hourNow > 18) {
+    greeting = 'Good evening!';
+  } else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+  } else if (hourNow > 0) {
+    greeting = 'Good morning!';
+  } else {
+    greeting = 'Welcome!';
+  }
+
+  // Insert the greeting into the div with id="greeting"
+  document.getElementById("greeting").innerHTML = `<p style="color: green;">${greeting}</p>`;
+});
+
+// Functions for opening and closing navigation
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
