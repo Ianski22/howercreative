09c1e48a5aa948cb1609aab7d188962dd6d6679f
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-02-11 13:18:58 -0700

    Update lesson78.js

--- lesson78.js
+++ lesson78.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-    var scrollSpeed = 20; // Adjust scroll speed
-    $("html, body").on("mousewheel", function(event, delta){
-        this.scrollLeft -= (delta * scrollSpeed); // Horizontal scroll
-        this.scrollTop -= (delta * scrollSpeed);  // Vertical scroll
-        event.preventDefault(); // Prevent default scrolling behavior
-    });
-});
+
