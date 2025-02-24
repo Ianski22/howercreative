d55cb5b959d2222251ac633ab93718d7c31d8df1
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-02-24 11:26:47 -0700

    Update lesson11.js

--- lesson11.js
+++ lesson11.js
@@ -1,22 +1,21 @@
-$('#tab-list').each(function () {
-    var $this = $(this); 
-    var $tab = $this.find('li.active');
-    var $link = $tab.find('a');
-    var $panel = $($link.attr('href'));
+$('.tab-list').each(function() {
+    var $this = $(this);                           // Store the tab list
+    var $tab = $this.find('li.active');            // Get the active tab
+    var $link = $tab.find('a');                    // Get link from active tab
+    var $panel = $($link.attr('href'));            // Get active panel
 
-    // When clicking on a tab
-    $this.on('click', '.tab-control', function (e) {
-        e.preventDefault(); // Prevent default link behavior
+    $this.on('click', '.tab-control', function(e) { // When clicking a tab
+        e.preventDefault();                        // Prevent link default behavior
+        
+        var $link = $(this);                       // Store the clicked link
+        var id = this.hash;                        // Get href (panel ID) of clicked tab
 
-        var $link = $(this); // Store the clicked link
-        var id = this.hash; // Get href of clicked tab
+        if (id && !$link.is('.active')) {          // If ID exists and link isn’t active
+            $panel.removeClass('active');          // Deactivate current panel
+            $tab.removeClass('active');            // Deactivate current tab
 
-        if (id && !$link.is('.active')) {
-            $panel.removeClass('active'); // Make current panel inactive
-            $tab.removeClass('active'); // Make current tab inactive
-
-            $panel = $(id).addClass('active'); // Make new panel active
-            $tab = $link.parent().addClass('active'); // Make new tab active
+            $panel = $(id).addClass('active');     // Activate new panel
+            $tab = $link.parent().addClass('active'); // Activate new tab
         }
     });
 });
