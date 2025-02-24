7580f9cfbeea1fe70238090aa9fc02d414100337
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-02-24 11:34:27 -0700

    Update lesson11.js

--- lesson11.js
+++ lesson11.js
@@ -1,16 +1,16 @@
 $('.tab-list').each(function() {
     var $this = $(this);                           // Store the tab list
     var $tab = $this.find('li.active');            // Get the active tab
-    var $link = $tab.find('a');                    // Get link from active tab
-    var $panel = $($link.attr('href'));            // Get active panel
+    var $link = $tab.find('a.tab-control');        // Get link from active tab
+    var $panel = $($link.attr('href'));            // Get active panel (e.g., #tab-photography)
 
-    $this.on('click', '.tab-control', function(e) { // When clicking a tab
-        e.preventDefault();                        // Prevent link default behavior
+    $this.on('click', '.tab-control', function(e) { // When clicking a tab link
+        e.preventDefault();                        // Stop default link behavior
         
-        var $link = $(this);                       // Store the clicked link
-        var id = this.hash;                        // Get href (panel ID) of clicked tab
+        var $link = $(this);                       // Store clicked link
+        var id = this.hash;                        // Get href (e.g., #tab-webdesign)
 
-        if (id && !$link.is('.active')) {          // If ID exists and link isn’t active
+        if (id && !$link.parent().is('.active')) { // If ID exists and tab isn’t active
             $panel.removeClass('active');          // Deactivate current panel
             $tab.removeClass('active');            // Deactivate current tab
 
