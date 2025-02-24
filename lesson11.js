22e536a648e744d2d4beb003f8b0f0e73a7be023
Ianski22 <121119165+Ianski22@users.noreply.github.com>
2025-02-24 11:40:07 -0700

    Update lesson11.js

--- lesson11.js
+++ lesson11.js
@@ -1,21 +1,19 @@
 $('.tab-list').each(function() {
-    var $this = $(this);                           // Store the tab list
-    var $tab = $this.find('li.active');            // Get the active tab
-    var $link = $tab.find('a.tab-control');        // Get link from active tab
-    var $panel = $($link.attr('href'));            // Get active panel (e.g., #tab-photography)
+    var $this = $(this);
+    var $activeTab = $this.find('li.active');
+    var $activeLink = $activeTab.find('a.tab-control');
+    var $activePanel = $($activeLink.attr('href'));
 
-    $this.on('click', '.tab-control', function(e) { // When clicking a tab link
-        e.preventDefault();                        // Stop default link behavior
-        
-        var $link = $(this);                       // Store clicked link
-        var id = this.hash;                        // Get href (e.g., #tab-webdesign)
+    $this.on('click', '.tab-control', function(e) {
+        e.preventDefault();
+        var $link = $(this);
+        var id = this.hash;
 
-        if (id && !$link.parent().is('.active')) { // If ID exists and tab isn’t active
-            $panel.removeClass('active');          // Deactivate current panel
-            $tab.removeClass('active');            // Deactivate current tab
-
-            $panel = $(id).addClass('active');     // Activate new panel
-            $tab = $link.parent().addClass('active'); // Activate new tab
+        if (id && !$link.parent().is('.active')) {
+            $activePanel.removeClass('active');
+            $activeTab.removeClass('active');
+            $activePanel = $(id).addClass('active');
+            $activeTab = $link.parent().addClass('active');
         }
     });
 });
